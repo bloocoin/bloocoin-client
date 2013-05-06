5438c8d7c0a8af15a3210f32eee89790aeb0fd67
Frankie <max00355@gmail.com>
2013-05-05 21:11:11 -0400

    transactions now shows amount sent

--- transactions.py
+++ transactions.py
@@ -19,6 +19,6 @@ def transactions():
     data = json.loads(data)
     for x in data['payload']['transactions']:
         if x['to'] == addr:
-            print "From: "+x['from']
+            print "From: "+x['from']+" "+str(x["amount"])
         if x['from'] == addr:
-            print "To: "+x['to']
+            print "To: "+x['to']+" "+str(x['amount'])
